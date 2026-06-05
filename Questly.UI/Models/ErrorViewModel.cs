67cf303cd297a5debac68a83abb2f9e456b3f252
reza-sjdn <reza.sjdn2001@gmail.com>
2026-06-05 22:25:09 +0330

    Start of the Questly project

---
+++ Questly.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Questly.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
